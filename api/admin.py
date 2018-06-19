e42b0fdc6b519ff7f28915a72040fb62e67bbeb6
Juliano Almeida da Silva | S2IT <juliano.silva@s2it.com.br>
2018-06-19 14:36:43 -0300

    admin changes

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Scheduling
+from .models import Scheduling, Patient, Procedure
 
 # Register your models here.
 admin.site.register(Scheduling)
+admin.site.register(Patient)
+admin.site.register(Procedure)
